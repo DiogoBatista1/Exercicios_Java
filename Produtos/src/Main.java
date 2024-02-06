a171827e67499e38d6394be6847f2396cbdc2de0
Diogo Batista <diogobatista330@gmail.com>
2024-02-05 21:46:25 -0300

    Added new exercice

--- Produtos/src/Main.java
+++ Produtos/src/Main.java
@@ -8,21 +8,25 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product product = new Product();
 		System.out.println("Enter the product data: ");
 		System.out.println("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.println("Price: ");
-		product.price = sc.nextDouble();
+		double price = sc.nextDouble();
 		System.out.println("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
+		
+		Product product = new Product(name, price, quantity);
+		
+		product.setName("Computador");
+		System.out.println(product.getName());
 		
 		System.out.println();
 		System.out.println("Product data: " + product);
 		
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
-		int quantity = sc.nextInt();
+		quantity = sc.nextInt();
 		product.addProducts(quantity);
 		
 		System.out.println();
--- Produtos/src/entities/Product.java
+++ Produtos/src/entities/Product.java
@@ -1,10 +1,45 @@
 package entities;
 
 public class Product {
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
 	
+	public Product() {
+		
+	}
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name ) {
+		this.name = name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price ) {
+		this.price = price;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
 	public double totalValueInStock() {
 		return quantity * price;
 	}
