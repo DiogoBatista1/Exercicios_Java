caa43f5d9b12a5dfb79868c85426aa15317d139f
Diogo Batista <diogobatista330@gmail.com>
2024-02-07 06:28:46 -0300

    Updated SaldoConta exercice

--- SaldoConta/src/Main.java
+++ SaldoConta/src/Main.java
@@ -1,4 +1,53 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Account acc = new Account();
+		
+		System.out.println("Enter account number: ");
+		Double accountNumber = sc.nextDouble();
+		
+		System.out.println("Enter account holder: ");
+		String name = sc.nextLine();
+		sc.nextLine();
+		
+		System.out.println("Is there a initial deposit? (y/n) ");
+		String theresDeposit = sc.nextLine();
+		
+		double initialDeposit = 0.0;
+		
+		if(theresDeposit.equalsIgnoreCase("y")) {
+            System.out.print("Enter initial deposit value: ");
+            initialDeposit = sc.nextDouble();
+		}
+		
+		acc.setName(name);
+		acc.setAccountNumber(accountNumber);
+		acc.setInitialDeposit(initialDeposit);
+		
+		System.out.println("Account data: ");
+		printAccData(acc);
+
+		System.out.println("Enter a deposit value: ");
+		double newDeposit = sc.nextDouble();
+		
+		acc.depositAcc(newDeposit);
+		
+		System.out.println("Updated account data:");
+		System.out.println(acc.getInitialDeposit());
+		
+		System.out.println();
+		
+		sc.close();
+	}
+	public static void printAccData(Account acc){
+        System.out.printf("Account %.0f, Holder: %s, Balance: %.2f%n", acc.getAccountNumber(), acc.getName(), acc.getInitialDeposit());
+	}
 
 }
---
+++ SaldoConta/src/entities/Account.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class Account {
+	public String name;
+	public double accountNumber;
+	public double initialDeposit;
+	public String theresDeposit;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(double accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public double getInitialDeposit() {
+		return initialDeposit;
+	}
+	public void setInitialDeposit(double initialDeposit) {
+		this.initialDeposit = initialDeposit;
+	}
+	
+	public void account(String name, double accNumber, double iniDeposit) {
+		name = getName();
+		accNumber = getAccountNumber();
+		iniDeposit = getInitialDeposit();
+		
+		return;
+	}
+	
+	public void depositAcc(double deposit) {
+		this.initialDeposit += deposit;
+		return;
+	}
+}
