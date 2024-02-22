875eaeb344941eb5b295ae284d2d644e9910abcd
Diogo Batista <diogobatista330@gmail.com>
2024-02-22 13:30:26 -0300

    Update code and identation

--- Pensionato/src/Entities/Room.java
+++ Pensionato/src/Entities/Room.java
@@ -15,25 +15,11 @@ public class Room {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getEmail() {
 		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	public int getRoomNumber() {
 		return roomNumber;
 	}
-
-	public void setRoomNumber(int roomNumber) {
-		this.roomNumber = roomNumber;
-	}
-	
-	
 }
--- Pensionato/src/Pensionato.java
+++ Pensionato/src/Pensionato.java
@@ -9,33 +9,33 @@ public class Pensionato {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Room rooms[] = new Room[9];
-		
-		rooms[0] = new Room("Diof", "Das", 1);
-		
 		System.out.println("How many rooms will be rented? ");
 		int rentedRooms = sc.nextInt();
+		sc.nextLine();
+		
+		Room rooms[] = new Room[rentedRooms];
 		
 		for(int i = 0; i < rentedRooms; i++) {
+			System.out.printf("Rent #%d: \n", i + 1);
+			
 			System.out.println("Name: ");
-			String name = sc.next();
-			rooms[i] = new Room(name, "", 0);
+			String name = sc.nextLine();
 			
 			System.out.println("Email: ");
-			String email = sc.next();
-			rooms[i].setEmail(email);
+			String email = sc.nextLine();
 			
 			System.out.println("Room: ");
 			int roomNum = sc.nextInt();
-			rooms[i].setRoomNumber(roomNum);
+			sc.nextLine(); 
+			
+			rooms[i] = new Room(name, email, roomNum);
 		}
+		
+		System.out.println("Busy rooms: ");
 		
 		for(int i = 0; i < rooms.length; i++) {
 			if(rooms[i] != null) {
-				System.out.println("Rent: ");
-				System.out.println("Name: " + rooms[i].getName());
-				System.out.println("Email: " +  rooms[i].getEmail());
-				System.out.printf("Room: %d\n" ,rooms[i].getRoomNumber());
+				System.out.printf("%d: %s, %s \n", rooms[i].getRoomNumber(), rooms[i].getName(), rooms[i].getEmail());
 			}
 		}
 
