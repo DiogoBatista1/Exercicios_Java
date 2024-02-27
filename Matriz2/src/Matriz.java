287752299478be3ae6cee9f6a2877e8dffc074b6
Diogo Batista <diogobatista330@gmail.com>
2024-02-26 21:02:21 -0300

    Added matriz2 exercice

---
+++ Matriz2/src/Matriz.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Matriz {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite os valores: ");
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int[][] mat = new int[x][y];
+		
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat[i].length; j++) {
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		sc.close();
+	}
+
+}
