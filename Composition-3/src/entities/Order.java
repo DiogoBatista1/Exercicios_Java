524d29c7e6d78a069bb0e0c4c3ae373d1e1c0dc4
Diogo Batista <diogobatista330@gmail.com>
2024-03-19 19:40:34 -0300

    Added constructor and getter/setter for Order class

--- Composition-3/src/entities/Order.java
+++ Composition-3/src/entities/Order.java
@@ -7,4 +7,25 @@ import entities.enums.OrderStatus;
 public class Order {
 	private Date moment;
 	private OrderStatus status;
+	
+	public Order(Date moment, OrderStatus status) {
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
 }
