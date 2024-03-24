1e7f2297ad4d19cf4fedaa588e645f3e9d4342bd
Diogo Batista <diogobatista330@gmail.com>
2024-03-23 21:13:51 -0300

    new commit

--- Inheritance-Java/src/Application/Program.java
+++ Inheritance-Java/src/Application/Program.java
@@ -1,5 +1,9 @@
-package Application;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
-
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+	}
 }
