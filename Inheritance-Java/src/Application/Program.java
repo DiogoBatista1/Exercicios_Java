470b98aefeb5b0517eff6f8f3a09d22a50528c85
Diogo Batista <diogobatista330@gmail.com>
2024-03-25 18:22:38 -0300

    Exercice inheritance added

--- Inheritance-Java/src/Application/Program.java
+++ Inheritance-Java/src/Application/Program.java
@@ -1,9 +1,51 @@
+package Application;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import Entities.Employee;
+import Entities.OutsourcedEmployee;
 
 public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of employees: ");
+		int n1 = sc.nextInt();
+		
+		for (int i=1; i<=n1; i++) {
+			System.out.println("Employee #" + i + " data:");
+			System.out.print("Outsourced (y/n)? ");
+			char ch = sc.next().charAt(0);
+			System.out.println("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine(); 
+			System.out.println("Hours: ");
+			int hours = sc.nextInt();
+			System.out.println("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			if(ch == 'y') {
+				System.out.println("Additional charge: ");
+				double additionalCharge = sc.nextDouble();
+				Employee emp = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+				list.add(emp);
+			} else {
+				Employee emp = new Employee(name, hours, valuePerHour);
+				list.add(emp);
+			}
+			
+			System.out.println();
+			System.out.println("PAYMENTS: ");
+			for(Employee emp : list) {
+				System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
+			}
+		}
+		
+		sc.close();
 	}
 }
--- Inheritance-Java/src/Entities/OutsourcedEmployee.java
+++ Inheritance-Java/src/Entities/OutsourcedEmployee.java
@@ -1,5 +1,26 @@
 package Entities;
 
-public class OutsourcedEmployee {
+public class OutsourcedEmployee extends Employee {
+	private double aditionalCharge;
 
+	public OutsourcedEmployee() {
+		super();
+	}
+	
+	public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double aditionalCharge) {
+		super(name, hours, valuePerHour);
+		this.aditionalCharge = aditionalCharge;
+	}
+
+	public double getAditionalCharge() {
+		return aditionalCharge;
+	}
+
+	public void setAditionalCharge(double aditionalCharge) {
+		this.aditionalCharge = aditionalCharge;
+	}
+	
+	public double payment() {
+		return super.payment() + aditionalCharge + 1.1;
+	}
 }
