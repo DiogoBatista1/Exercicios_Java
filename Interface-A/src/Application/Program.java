c64db8e4666d8bc578d0689d10f0fadae0e2afa1
Diogo Batista <diogobatista330@gmail.com>
2024-05-09 11:34:49 -0300

    interface comparable

---
+++ Interface-A/src/Application/Program.java
@@ -0,0 +1,34 @@
+package Application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import entities.Employee;
+
+public class Program {
+	public static void main(String[] args) {
+		List<Employee> list = new ArrayList<>();
+		
+		String path = "src\\in.txt";
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employeeCsv = br.readLine();
+			}
+//			Ordena lista em ordem alfabetica
+			Collections.sort(list);
+			
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", " + emp.getSalary());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
