296cd8f16d0a894ec87a0610d160737a5f732019
Diogo Batista <diogobatista330@gmail.com>
2024-06-13 06:18:52 -0300

    added jdbc5 class demo project

---
+++ jdbc5/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import DB.DbIntegrityException;
+
+import DB.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.prepareStatement(
+					"DELETE FROM department "
+					+ "WHERE "
+					+ "Id = ?"
+					);
+			st.setInt(1, 2);
+			
+			int rowsAffected = st.executeUpdate();
+
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		} catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.CloseConnection();
+		}
+	}
+}
+
